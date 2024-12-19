dd4cec701d765144139e3ee4c8f0b69de0e0606f
AmdadulOrbitax <amdadul.haque@orbitax.com>
2024-12-19 19:05:53 +0600

    Add files via upload

---
+++ E_commerce/E_commerce/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace E_commerce.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; } = string.Empty;
+        public string UserName { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; }
+        public string Roles { get; set; }
+    }
+}
